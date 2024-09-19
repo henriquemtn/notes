869937334a04505158e468b74e252280f3553d3a
Henrique Silveira <henriquemartinsilveira2@gmail.com>
2024-09-19 15:35:53 -0300

    add: "document-list.tsx and implementings calc in levels for documents"

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,15 +4,15 @@ import { usePathname } from 'next/navigation';
 import React, { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 import { UserItem } from './user-item';
-import { useMutation, useQuery } from 'convex/react';
+import { useMutation } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { Item } from './item';
 import { toast } from 'sonner';
+import { DocumentList } from './document-list';
 
 export default function Navigation() {
     const pathname = usePathname();
     const isMobile = useMediaQuery('(max-width: 768px)');
-    const documents = useQuery(api.documents.get);
     const create = useMutation(api.documents.create);
 
 
@@ -143,11 +143,7 @@ export default function Navigation() {
                     icon={PlusCircle}
                 />
                 <div className='mt-4'>
-                    {documents?.map((document) => (
-                        <p key={document._id}>
-                            {document.title}
-                        </p>
-                    ))}
+                    <DocumentList />
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
